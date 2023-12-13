b79f097acd682a27b9433ae1607b382ee82db138
null <null>
2023-12-13 12:16:37 -0600

    add main test

---
+++ test/main.test.js
@@ -0,0 +1,9 @@
+import test from "node:test"
+import assert from "node:assert/strict";
+import { $ } from "execa";
+import { fileURLToPath } from "node:url";
+
+test("main.js --help", async () => {
+    const mainPath = fileURLToPath(new URL("../dist/main.js", import.meta.url))
+    await $({ stdio: "inherit" })`${process.execPath} ${mainPath} --help`
+})
