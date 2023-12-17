117f3a0c1df330a567df74334833f773fe830771
Jacob Hummer <jcbhmr@outlook.com>
2023-12-17 17:19:44 -0600

    Update compile.ts

--- src/compile.ts
+++ src/compile.ts
@@ -1,9 +1,6 @@
 import { PathLike } from "node:fs";
 import { toPath, typstPath } from "./utils.js";
 import { $ } from "execa";
-import { query } from './query';
-
-
 
 export interface TypstCompileOptions {
   root?: PathLike;
@@ -34,21 +31,4 @@ export default async function compile(
   } else {
     await $`${typstPath} compile ${opts} ${inputPath} ${outputPath}`;
   }
-}
-
-export async function query(
-  inputRaw: PathLike,
-  selectorRaw: PathLike,
-  options: TypstQueryOptions = {},
-) {
-  const inputPath = toPath(inputRaw);
-  const selectorPath = toPath(selectorRaw);
-  const opts = [];
-  if (options.root != null) opts.push("--root", toPath(options.root));
-  if (options.fontPath != null) opts.push("--font-path", toPath(options.fontPath));
-  if (options.diagnosticFormat != null) opts.push("--diagnostic-format", options.diagnosticFormat);
-  if (options.field != null) opts.push("--field", options.field);
-  if (options.one != null) opts.push("--one");
-  if (options.format != null) opts.push("--format", options.format);
-  await $`${typstPath} query ${opts} ${inputPath} ${selectorPath}`;
 }
